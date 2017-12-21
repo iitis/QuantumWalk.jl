28d747236261506c808edd66d240aa91dd2e600d
autodocs <autodocs>
2017-12-21 11:23:05 +0000

    build based on bb913c3

---
+++ latest/search_index.js
@@ -0,0 +1,75 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Basics",
+    "title": "Basics",
+    "category": "page",
+    "text": "Welcom to QSpatialSearch.jl package."
+},
+
+{
+    "location": "type_hierarchy.html#",
+    "page": "Quantum walk evolution types",
+    "title": "Quantum walk evolution types",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "quantum_search.html#",
+    "page": "Quantum search",
+    "title": "Quantum search",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "quantum_walk.html#",
+    "page": "Quantum walk simulator",
+    "title": "Quantum walk simulator",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "special_models.html#",
+    "page": "Examplary models",
+    "title": "Examplary models",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "own_model.html#",
+    "page": "How to make your own model?",
+    "title": "How to make your own model?",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "license.html#",
+    "page": "Licence",
+    "title": "Licence",
+    "category": "page",
+    "text": "MIT LicenseCopyright (c) 2017 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
+},
+
+{
+    "location": "citing.html#",
+    "page": "Citing",
+    "title": "Citing",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "contributing.html#",
+    "page": "Contributing",
+    "title": "Contributing",
+    "category": "page",
+    "text": ""
+},
+
+]}
