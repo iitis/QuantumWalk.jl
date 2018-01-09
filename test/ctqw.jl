a7cc5232e51e3e94db79981b105901fdef75337b
Jarosław Miszczak <miszczak@iitis.pl>
2018-01-09 22:57:44 +0100

    Last test for ctqw

--- test/ctqw.jl
+++ test/ctqw.jl
@@ -1,11 +1,12 @@
 @testset "CTQW model check" begin
   n = 5
   g = CompleteGraph(n)
+  ctqw = CTQW(g)
+  @test QuantumWalk.matrix(ctqw) == ctqw.matrix
 
   @testset "Default jumping rate" begin
-    @test QuantumWalk.jumping_rate(CTQW(g)) ≈ 1./(n-1)
+    @test QuantumWalk.jumping_rate(ctqw) ≈ 1./(n-1)
     @test QuantumWalk.jumping_rate(CTQW(Graph(n))) == 1.
     @test_throws AssertionError QuantumWalk.jumping_rate(CTQW(g, :laplacian))
-
   end
 end
