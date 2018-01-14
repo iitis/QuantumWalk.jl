4e1ccab91da5847f104647862a7e04a97b45ee75
Jarosław Miszczak <miszczak@iitis.pl>
2018-01-14 16:25:00 +0100

    Testing the measurement

--- test/ctqw.jl
+++ test/ctqw.jl
@@ -2,7 +2,11 @@
   n = 5
   g = CompleteGraph(n)
   ctqw = CTQW(g)
+  ctqw_qss = QWSearch(ctqw, [1,3])
+
+  @test isapprox(sum(measure(ctqw_qss, initial_state(ctqw_qss))), 1.0)
   @test QuantumWalk.matrix(ctqw) == ctqw.matrix
+
 
   @testset "Default jumping rate" begin
     @test QuantumWalk.jumping_rate(ctqw) ≈ 1./(n-1)
