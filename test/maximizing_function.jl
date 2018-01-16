e6cc7c1e695de82d3eb8a167cd849ab399945369
Jarosław Miszczak <miszczak@iitis.pl>
2018-01-16 21:03:15 +0100

    Started tests for maximizing_function.jl

--- test/maximizing_function.jl
+++ test/maximizing_function.jl
@@ -3,5 +3,5 @@
     g = CompleteGraph(n)
     qss = QWSearch(CTQW(g), [1])
 
-    @test_throws AssertionError maximize_quantum_search(qss, 2.0, -0.1)
+    @test_throws AssertionError maximize_quantum_search(qss, -1.)
 end
