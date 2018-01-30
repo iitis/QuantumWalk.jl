c828e70624e24acdccd49b7667689c50306e29d9
Jarosław Miszczak <miszczak@iitis.pl>
2018-01-30 19:56:51 +0100

    Testing penalty

--- test/maximizing_function.jl
+++ test/maximizing_function.jl
@@ -1,7 +1,8 @@
 @testset "Maximization for quantum search" begin
     n = 5
     g = CompleteGraph(n)
-    qss = QWSearch(CTQW(g), [1])
+    qss = QWSearch(CTQW(g), [1], nv(g), ceil(Int, log(nv(g))))
 
     @test_throws AssertionError maximize_quantum_search(qss, -1.)
+    @test runtime(maximize_quantum_search(qss)) > ceil(Int, log(nv(g)))
 end
