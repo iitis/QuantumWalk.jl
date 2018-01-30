a39fa05dda83af47545529df0543301ee945b918
Jarosław Miszczak <miszczak@iitis.pl>
2018-01-30 20:56:41 +0100

    Tests for modes of execution

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,9 @@ using QuantumWalk
 using LightGraphs
 using Base.Test
 
-#include("ctqw.jl")
-#include("szegedy.jl")
+include("ctqw.jl")
+include("szegedy.jl")
 include("qwsearch.jl")
-#include("qwevolution.jl")
-#include("type_hierarchy.jl")
-#include("maximizing_function.jl")
+include("qwevolution.jl")
+include("type_hierarchy.jl")
+include("maximizing_function.jl")
