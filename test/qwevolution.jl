5824f9e2685a4349b82704ef4746dde3d12e3750
Jarosław Miszczak <miszczak@iitis.pl>
2018-02-27 21:54:47 +0100

    Added tests for modes of execution

--- test/qwevolution.jl
+++ test/qwevolution.jl
@@ -2,9 +2,10 @@
 
   n = 5
   g = CompleteGraph(n)
-  init = fill(Complex128(1/sqrt(n)), n)
+
 
   @testset "Quantum walks simulator for CTQW" begin
+    init = fill(Complex128(1/sqrt(n)), n)
     qwe = QWEvolution(CTQW(g, :adjacency))
 
     @test_throws AssertionError execute(qwe, init , -1.0)
@@ -13,8 +14,17 @@
 
   @testset "Quantum walks simulator for Szegedy" begin
     qwe = QWEvolution(Szegedy(g))
+    init = spzeros(n*n)
+    init[1] = init[n] = init[n*n] = 1/sqrt(3.)
+    steps = 10
+
     @test_throws AssertionError execute(qwe, init, -1)
     @test_throws MethodError execute(qwe, init, 0.5)
+
+    @test execute(qwe, init, steps, all = true, measure = true)[:,steps+1] == execute(qwe, init, steps, all = false, measure = true)
+    @test execute(qwe, init, steps, all = true, measure = false)[steps+1] == execute(qwe, init, steps, all = false, measure = false)
+
   end
+
 
 end
